18552adfb4c6b4505b67557c67f72d0e56a4ff10
edo oktarifa <edooktarifa99@gmail.com>
2022-05-19 09:09:13 +0700

    Initial commit

---
+++ Example/CommonLibHelperCapstone/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CommonLibHelperCapstone
+//
+//  Created by edooktarifa99@gmail.com on 05/19/2022.
+//  Copyright (c) 2022 edooktarifa99@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
